40574db3efa7848bffba21a1c154524e3139f6f1
oscarotero <oom@oscarotero.com>
2016-11-27 13:24:31 +0100

    updated http-middleware interface to 0.3

--- tests/AuraSessionTest.php
+++ tests/AuraSessionTest.php
@@ -3,9 +3,10 @@
 namespace Middlewares\Tests;
 
 use Middlewares\AuraSession;
+use Middlewares\Utils\Dispatcher;
+use Middlewares\Utils\CallableMiddleware;
 use Zend\Diactoros\ServerRequest;
 use Zend\Diactoros\Response;
-use mindplay\middleman\Dispatcher;
 
 class AuraSessionTest extends \PHPUnit_Framework_TestCase
 {
@@ -13,14 +14,15 @@ class AuraSessionTest extends \PHPUnit_Framework_TestCase
     {
         $response = (new Dispatcher([
             (new AuraSession())->name('custom-session'),
-            function ($request) {
+            new CallableMiddleware(function ($request) {
                 $session = $request->getAttribute('session');
                 $this->assertInstanceOf('Aura\\Session\\Session', $session);
 
                 $response = new Response();
                 $response->getBody()->write($session->getName());
+
                 return $response;
-            },
+            }),
         ]))->dispatch(new ServerRequest());
 
         $this->assertInstanceOf('Psr\\Http\\Message\\ResponseInterface', $response);
