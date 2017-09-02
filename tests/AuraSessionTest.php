c4c211da41e8176b022d881e99ababc7e6c3b63a
oscarotero <oom@oscarotero.com>
2017-09-02 18:24:50 +0200

    Updated dependencies

--- tests/AuraSessionTest.php
+++ tests/AuraSessionTest.php
@@ -2,11 +2,12 @@
 
 namespace Middlewares\Tests;
 
+use PHPUnit\Framework\TestCase;
 use Middlewares\AuraSession;
 use Middlewares\Utils\Dispatcher;
 use Middlewares\Utils\Factory;
 
-class AuraSessionTest extends \PHPUnit_Framework_TestCase
+class AuraSessionTest extends TestCase
 {
     public function testAuraSession()
     {
