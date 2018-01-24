891930470b4c32627e5b9c5af689917004e0d15b
oscarotero <oom@oscarotero.com>
2018-01-24 21:21:16 +0100

    Code style fix

--- src/AuraSession.php
+++ src/AuraSession.php
@@ -5,10 +5,10 @@ namespace Middlewares;
 
 use Aura\Session\Session;
 use Aura\Session\SessionFactory;
-use Psr\Http\Server\MiddlewareInterface;
-use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
 class AuraSession implements MiddlewareInterface
 {
